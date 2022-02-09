6663e7a5c906e014db549ac1f8e6e1ea8f4ced0f
LrnzCode <73407537+lrnzcode@users.noreply.github.com>
2022-02-09 18:54:47 +0100

    Add files via upload

---
+++ NetflixDiscordStatus/Misc/DragControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace SeleniumTest
+{
+    class DragControl : Component
+    {
+        private Control handleControl;
+
+        public Control SelectControl
+        {
+            get
+            {
+                return this.handleControl;
+            }
+            set
+            {
+                this.handleControl = value;
+                this.handleControl.MouseDown += new MouseEventHandler(this.OnDrag);
+            }
+        }
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr a, int msg, int with, int lenght);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void OnDrag(object sender, MouseEventArgs e)
+        {
+            bool flag = e.Button == MouseButtons.Left;
+            if (flag)
+            {
+                DragControl.ReleaseCapture();
+                DragControl.SendMessage(this.SelectControl.FindForm().Handle, 161, 2, 0);
+            }
+        }
+    }
+}
