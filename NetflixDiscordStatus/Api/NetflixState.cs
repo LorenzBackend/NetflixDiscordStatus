b479a9de34718aa4488510377d1b6ba0eb218b16
Lorenz Backend <73407537+LorenzBackend@users.noreply.github.com>
2022-02-23 03:45:35 +0100

    Update NetflixState.cs

--- NetflixDiscordStatus/Api/NetflixState.cs
+++ NetflixDiscordStatus/Api/NetflixState.cs
@@ -4,31 +4,37 @@ using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetflixDiscordStatus.Api
 {
-    //NetflixDiscordStatus by LrnzCode
-    //https://github.com/lrnzcode/NetflixDiscordStatus
+    //NetflixDiscordStatus by LorenzBackend
+    //https://github.com/LorenzBackend/NetflixDiscordStatus
     public class NetflixState
     {
         private static DiscordRpcClient client;
+        private static DiscordRpcClient client2;
+
         private static string[] states = { "Browse the Netflix world" };
         private static RpcState currentState;
         private static string currentMovie = "";
         private enum RpcState
         {
             Browse,
-            Move,
-            Series
+            Movie,
+            Series,
+            None
         }
         public static void Init()
         {
+
             try
             {
-                client = new DiscordRpcClient("ClientId");
+                currentState = RpcState.None;
+                client = new DiscordRpcClient("940447627282104330");
                 client.Initialize();
                 SetBrowseState();
 
@@ -43,13 +49,22 @@ namespace NetflixDiscordStatus.Api
 
         private static void SetBrowseState()
         {
-            currentState = RpcState.Browse;
             currentMovie = "";
+            Timestamps time;
+            if (client.CurrentPresence != null)
+            {
+                time = client.CurrentPresence.Timestamps;
+            }
+            else
+            {
+                time = Timestamps.Now;
+            }
+
             client.SetPresence(new RichPresence()
             {
                 State = "",
                 Details = states[0],
-                Timestamps = Timestamps.Now,
+                Timestamps = time,
                 Assets = new Assets()
                 {
                     LargeImageKey = "app_icon"
@@ -59,6 +74,8 @@ namespace NetflixDiscordStatus.Api
                     new Button() { Label = "GET THE APP", Url = "https://github.com/lrnzcode/NetflixDiscordStatus/releases" }
                 }       
             });
+
+            currentState = RpcState.Browse;
         }
 
         private static void CheckForNewState()
@@ -103,35 +120,57 @@ namespace NetflixDiscordStatus.Api
                         {
                             string movieTitleText = movieTitle.Text;
                             if (movieTitleText.Length > 32) movieTitleText = movieTitleText.Substring(0, 32);
-                            currentState = RpcState.Move;
+                            currentState = RpcState.Movie;
                             UpdateState("Watching", movieTitleText);
                         }
                         catch
                         {
                             //hm i dont know... do nothing
-                        }
-             
+                        }  
+                    }
+                }
+
+
+
+                if (currentState == RpcState.Movie || currentState == RpcState.Series)
+                {
+                    IWebElement playerState = Core.FindElementWhenExists(By.CssSelector(".svg-icon-nfplayerPause > path"));
+
+                    if (playerState != null)
+                    {
+
                     }
                 }
             }
         }
 
+ 
         public static void UpdateState(string detail, string state)
         {
-            if (currentState == RpcState.Move && state == currentMovie) return;
+            if (currentState == RpcState.Movie && state == currentMovie) return;
             if (currentState == RpcState.Series && state == currentMovie) return;
 
-            if (currentState == RpcState.Move) currentMovie = detail;
+            if (currentState == RpcState.Movie) currentMovie = detail;
             if (currentState == RpcState.Series) currentMovie = state;
 
             client.UpdateState(state);
             client.UpdateDetails(detail);
 
+            Timestamps time;
+            if (client.CurrentPresence != null)
+            {
+                time = client.CurrentPresence.Timestamps;
+            }
+            else
+            {
+                time = Timestamps.Now;
+            }
+
             client.SetPresence(new RichPresence()
             {
                 State = state,
                 Details = detail,
-                Timestamps = Timestamps.Now,
+                Timestamps = time,
                 Assets = new Assets()
                 {
                     LargeImageKey = "app_icon"
